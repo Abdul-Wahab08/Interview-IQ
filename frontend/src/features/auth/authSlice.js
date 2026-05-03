1307aa8fe29b7ad9b79e2dc7f9388bdd046d5efe
Abdul-Wahab08 <abdulwahab3929706@gmail.com>
2026-05-03 14:30:20 +0500

    refactor authSlice reducer formatting for consistency

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -9,17 +9,17 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    login: (state, action)=>{
-        state.userData = action.payload
-        state.isAuthenticated = true
+    login: (state, action) => {
+      state.userData = action.payload
+      state.isAuthenticated = true
     },
-    logout: (state)=>{
-        state.userData = null
-        state.isAuthenticated = false
+    logout: (state) => {
+      state.userData = null
+      state.isAuthenticated = false
     }
   }
 })
 
-export const { login, logout } = authSlice.actions  
+export const { login, logout } = authSlice.actions
 
 export default authSlice.reducer
