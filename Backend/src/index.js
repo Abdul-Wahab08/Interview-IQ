dbb3befe1d6d7384e08f995537fdb717c111e7a8
Abdul-Wahab08 <abdulwahab3929706@gmail.com>
2026-05-08 21:46:08 +0500

    update index.js for vercel deployment

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,9 @@
 import express from "express"
 import { dbConnect } from "./db/dbConnect.js"
 import app  from "./app.js"
-import dotenv from "dotenv"
+// import dotenv from "dotenv"
 
-dotenv.config({path: './.env'})
+// dotenv.config({path: './.env'})
 
 const port = process.env.PORT
 
