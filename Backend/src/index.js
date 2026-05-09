13a81d488d640386a9827ae4d6486fb0af2a1168
Abdul-Wahab08 <abdulwahab3929706@gmail.com>
2026-05-09 20:02:11 +0500

    fix: remove app.listen for Vercel serverless

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,20 +1,15 @@
 import express from "express"
 import { dbConnect } from "./db/dbConnect.js"
-import app  from "./app.js"
+import app from "./app.js"
 // import dotenv from "dotenv"
 
 // dotenv.config({path: './.env'})
 
 const port = process.env.PORT
 
- dbConnect()
+dbConnect()
     .then(() => {
-        app.listen(port, () => {
-            console.log(`⚙️  Server running on port ${port}`)
-        })
-        app.on("Error", (error) => {
-            console.log("Error ", error)
-        })
+        console.log(`⚙️  Server running on port ${port}`)
     })
     .catch((error) => {
         console.log("MongoDB Connection Failed ", error)
